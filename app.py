2bd3647f3da0fc3b0c60ba8c7a6b83e93bc99918
tabercwebb <52960785+tabercwebb@users.noreply.github.com>
2019-12-01 16:26:12 -0800

    Updated app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import scrape_mars
 # Create an instance of Flask
 app = Flask(__name__)
 
-# Use flask_pymongo to set up mongo connection
+# Use Flask_Pymongo to Set Up Mongo Connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
 mongo = PyMongo(app)
 
